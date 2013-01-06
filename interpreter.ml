1b3f4d8a014a251d5ee910e06e2b751f724af17d
darkf <lw9k123@gmail.com>
2013-01-05 16:32:14 -0800

    add arithmetic test and + and int->str

--- interpreter.ml
+++ interpreter.ml
@@ -6,34 +6,17 @@ let genv : env = newEnv None
 
 let (|<) f v = f v
 
-let runTests () =
-	let test_env () =
-	  assert ((Hashtbl.length genv.sym) = 0);
-	  setSymLocal genv "x" (Int 10);
-	  assert ((Hashtbl.length genv.sym) = 1);
-	  assert (Hashtbl.mem genv.sym "x");
-	  (match lookup genv "x" with
-	  | Some (Int 10) -> ()
-	  | _ -> assert false);
-	  let n = newEnv (Some genv) in
-	  setSymLocal n "y" (Int 20);
-	  assert ((Hashtbl.length n.sym) = 1);
-	  (match lookup n "y" with
-	   		| Some (Int 20) -> ()
-	   		| _ -> assert false);
-	  (match lookup n "x" with
-			| Some (Int 10) -> ()
-			| _ -> assert false);
-	  assert (setSymFar n "x" (Int 50));
-	  (match lookup n "x" with
-			| Some (Int 50) -> ()
-			| _ -> assert false)
-    in
-    test_env ()
-
 let _printfn = function
 	| [Str x] -> printf ": %s\n" x; Nil
 	| _ -> failwith "print error"
+
+let _plus = function
+	| [Int lhs; Int rhs] -> Int (lhs+rhs)
+	| _ -> failwith "not integers"
+
+let _int_to_str = function
+	| [Int i] -> Str (string_of_int i)
+	| _ -> failwith "not an integer"
 
 let rec evalNode env node = match node with
 	| Call (name, args) ->
@@ -55,11 +38,56 @@ let rec eval env = function
 	| x::xs ->
 		evalNode  env x
 
-let () =
-	runTests (); (* sanity tests *)
-
+let setupStdlib () =
 	setSymLocal genv "print" |< Fun ([Atom "str"], _printfn);
+	setSymLocal genv "+" |< Fun ([Atom "lhs"; Atom "rhs"], _plus);
+	setSymLocal genv "int->str" |< Fun ([Atom "x"], _int_to_str)
+
+let runTests () =
+	let reset () =
+	  (* empty the symtable and repopulate it *)
+      Hashtbl.reset genv.sym;
+      setupStdlib ()
+    in
+	let test_env () =
+	  setSymLocal genv "x" (Int 10);
+	  assert (Hashtbl.mem genv.sym "x");
+	  (match lookup genv "x" with
+	  | Some (Int 10) -> ()
+	  | _ -> assert false);
+	  let n = newEnv (Some genv) in
+	  setSymLocal n "y" (Int 20);
+	  (match lookup n "y" with
+	   		| Some (Int 20) -> ()
+	   		| _ -> assert false);
+	  (match lookup n "x" with
+			| Some (Int 10) -> ()
+			| _ -> assert false);
+	  assert (setSymFar n "x" (Int 50));
+	  (match lookup n "x" with
+			| Some (Int 50) -> ()
+			| _ -> assert false)
+    in
+    let test_arithmetic () =
+      let thirty = Call ("+", [Int 10; Int 20]) in
+      let p = Call ("int->str", [thirty]) in
+      assert ((evalNode genv thirty) = (Int 30));
+      assert ((evalNode genv p) = (Str "30"))
+    in
+
+    test_env ();
+    reset ();
+    test_arithmetic ();
+    reset ()
+
+let () =
+	setupStdlib ();
+	runTests (); (* sanity tests *)
 	print_env 0 genv;
 
-	let program = [Call ("print", [Str "hi"])] in
+	let program = [Call ("print", [
+									Call ("int->str", [
+										Call ("+", [Int 10; Int 20])
+									])
+								  ])] in
 	print_node 0 (eval genv program)
